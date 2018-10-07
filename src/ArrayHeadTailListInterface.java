3384273f069f932febfb7d29c4335579872c9056
wsmarshall <42559252+wsmarshall@users.noreply.github.com>
2018-10-07 15:59:30 -0700

    constructors, clear, and get entry done. feel free to look over

--- src/ArrayHeadTailListInterface.java
+++ src/ArrayHeadTailListInterface.java
@@ -1,23 +1,32 @@
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ArrayHeadTailListInterface<T> implements HeadTailListInterface<T> {
-	
+
 	private T[] listArray;
 	private int numberOfElements;
-	
-	public ArrayHeadTailListInterface (int initial_capacity){
-		
+	private static final int DEFAULT_CAPACITY = 25;
+
+	public ArrayHeadTailListInterface() {
+		this(DEFAULT_CAPACITY);
 	}
-	
+
+	public ArrayHeadTailListInterface(int initial_capacity) {
+		listArray = (T[]) new Array[initial_capacity - 1]; //cast should be fine because the initialized Array is populated with null entries
+		numberOfElements = initial_capacity;
+	}
+
 	@Override
 	public void addFront(T newEntry) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void addBack(T newEntry) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -32,22 +41,30 @@ public class ArrayHeadTailListInterface<T> implements HeadTailListInterface<T> {
 		return null;
 	}
 
+	// mine
 	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
-		
+		T[] tempArray = (T[]) new Array[numberOfElements];
+		listArray = (T[]) tempArray;
 	}
 
+	// mine
 	@Override
-	public T getEntry(int givenPosition) {
-		// TODO Auto-generated method stub
-		return null;
+	public T getEntry(int givenPosition) throws IllegalArgumentException {
+		if (this.isEmpty()) {
+			throw new IllegalArgumentException("Not a valid position - this structure appears to be empty.");
+		} else if (listArray[givenPosition] == null) {
+			throw new IllegalArgumentException("Not a valid position - the value is null.");
+		} else {
+			return listArray[givenPosition];
+		}
 	}
 
 	@Override
 	public void display() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
